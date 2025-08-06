9104bc39dc339bf941ff7447751fcc7a1430a9c1
Rittaaa <gainju.rita123@gmail.com>
2025-08-06 17:24:43 +0545

    route groups and layouts

---
+++ src/app/_lib/footer.tsx
@@ -0,0 +1,5 @@
+export default function Footer() {
+  return (
+    <h1 style={{ backgroundColor: "ghostWhite", padding: "1rem" }}> Footer</h1>
+  );
+}
---
+++ src/app/_lib/header.tsx
@@ -0,0 +1,5 @@
+export default function Header() {
+  return (
+    <h1 style={{ backgroundColor: "lightblue", padding: "1rem" }}> Header</h1>
+  );
+}
